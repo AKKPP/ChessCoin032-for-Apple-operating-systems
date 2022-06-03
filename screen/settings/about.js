c5e4d82b0ef2a312ec5882b8ee64504371c8f68f
OleksandrAAA <aleksandkolesnik73@gmail.com>
2022-06-03 10:44:38 +0700

    Fixed git url for about.

--- screen/settings/about.js
+++ screen/settings/about.js
@@ -87,7 +87,7 @@ const About = () => {
     Linking.openURL('https://t.me/joinchat/-I8F2uaxZSRkZDcy');
   };
   const handleOnGithubPress = () => {
-    Linking.openURL('https://github.com/OleksandrAAA/ChessCoin032-Wallet');
+    Linking.openURL('https://github.com/OleksandrAAA/ChessCoin032-Wallet-ios');
   };
   const handleOnWebsitePress = () => {
     Linking.openURL('https://www.chesscoin032.com/');
